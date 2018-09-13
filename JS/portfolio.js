cab63eb4e7a1959637877ab082efecd7067aa17b
VincentGrice <39641488+vgrice888@users.noreply.github.com>
2018-09-13 13:45:18 -0400

    update

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,18 +1,3 @@
-/*--Word Changer--*/
-var text = ["",""];
-              var counter = 0;
-              var elem = document.getElementById("changeText");
-              var inst = setInterval(change, 1500);
-
-              function change() {
-                elem.innerHTML = text[counter];
-                counter++;
-                if (counter >= text.length) {
-                  counter = 0;
-    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
-  }
-}
-
 /*--Animated headline--*/
 
 
